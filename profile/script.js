b8176b25558a36c7e2fe6482b041c55dc62dbda0
Pushkar Sharma <80835222+sharmapushkar-coder@users.noreply.github.com>
2024-03-24 07:40:49 +0530

    Create script.js

---
+++ profile/script.js
@@ -0,0 +1,69 @@
+var database=firebase.database();
+var user_bar_name=window.location.search
+var user_bar=user_bar_name.replace('?u=','');
+var active
+var name ="Unable to find the requested user , Make sure this account really exist <a href=''>Try Again</a>"
+var clas
+var insta
+var made
+var page
+var plan
+database.ref('users/'+user_bar).on("value",function(snap){
+  plan=snap.val().plan
+})
+setTimeout(function(){
+  if(typeof plan==="undefined"||plan===null||plan===""){
+    document.getElementById("premium").innerHTML="Basic User"
+  }
+  else if(plan==="gold"){
+    document.body.style.backgroundImage = "url('gold.jpg')";
+    document.getElementById("premium").innerHTML="Golden Member"
+  }
+  else if(plan==="diamond"){
+    document.body.style.backgroundImage = "url('diamond.jpg')";
+    document.getElementById("premium").innerHTML="Diamond Member"
+  }
+},5000)
+console.log("Fetching details of "+user_bar)
+setTimeout(function(){document.getElementById("username").innerHTML=user_bar},2000)
+
+document.title=user_bar+"-Profile"
+
+database.ref("users/"+user_bar).on("value",function(snap){
+  name=snap.val().name,
+  made=snap.val().Created
+})
+setTimeout(function(){
+  if(typeof name==="undefined"||name===null||name===""){
+   alert("User not found")
+   window.history.back();
+  }
+ else{
+   document.getElementById("name").innerHTML="Name - "+name
+   document.getElementById("since").innerHTML="Joined : "+made
+  }
+},5000)
+database.ref("users/"+user_bar+"/social").on("value",function(soop){
+  insta=soop.val().instagram
+})
+setTimeout(function(){
+  if(typeof insta==="undefined"||insta===null||insta===""){
+    
+  }
+  else{
+    document.getElementById("social").innerHTML+="<a href='"+insta+"'> Follow "+user_bar+" on Instagram</a>"
+  }
+},5000)
+function search(){
+  var user=document.getElementById("inputuser").value;
+  location.href="https://connectopia.repl.co/profile/?u="+user
+}
+
+
+setInterval(function(){
+  database.ref("users/"+user_bar+"/online").on("value",function(shot){
+    active=shot.val().last,
+    page=shot.val().page
+  })
+  document.getElementById("laston").innerHTML="Last active  : "+active+" On :"+page
+})
