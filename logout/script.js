9b249d6bd2243dca2fc96b349ff357c3a995ac87
Pushkar Sharma <80835222+sharmapushkar-coder@users.noreply.github.com>
2024-03-27 23:59:11 +0530

    Create script.js

---
+++ logout/script.js
@@ -0,0 +1,17 @@
+var server= firebase.database();
+var token=localStorage.getItem('authid')
+function logout(){
+  if(token===""){
+    location.href='https://sharmapushkar-coder.github.io/Disclosure/?F=lgo&response=100'
+  }
+  else{
+    server.ref('login/'+token).set({
+      'user':null
+    })
+    setTimeout(function(){
+      alert("Logged Out successfully")
+      localStorage.removeItem("authid")
+      location.href="https://sharmapushkar-coder.github.io/Disclosure/?from=logout&&result=khudcheckkarlo"
+    },5000)
+  }
+}
