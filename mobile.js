04c1acab90e88185b877c62efbede73571f8d8e2
Pushkar Sharma <80835222+sharmapushkar-coder@users.noreply.github.com>
2024-04-07 14:25:13 +0530

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -4,6 +4,21 @@ var uname,width,height
 var device
 var home_visit=0;
 var code=window.location.search;
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Cloud Messaging and get a reference to the service
+const messaging = getMessaging(app);
 if(code==="?error=242"){
   setTimeout(function(){
     document.getElementById("info").style.display="block"
