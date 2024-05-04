af4976abbf777752318c50177581dc19c7ac975b
Pushkar Sharma <80835222+sharmapushkar-coder@users.noreply.github.com>
2024-05-04 19:11:16 +0530

    Update script.js

--- account/script.js
+++ account/script.js
@@ -2,3 +2,15 @@ console.log("Donot send any info shown in the delevoper tools, Your account migh
 var database=firebase.database();
 var token=localStorage.getItem("authid")
 console.log("Connecting to the auth id ::::: "+token+" (Donot copy this!!!!!)")
+var user="Not found <a href=''>Try Again</a>"
+var name=document.getElementById("name")
+var username=document.getElementById("username")
+var dob=document.getElementById("dob")
+var join=document.getElementById("join")
+var mail=document.getElementById("email")
+setTimeout(function(){
+  database.ref("login/"+token).on("value",function(snap){
+    user=snap.val().user
+  })
+  name.innerHTML=user
+},3000)
