afd3472944148cae32dd9ec24e3f2f62cfc128db
Pushkar Sharma <80835222+sharmapushkar-coder@users.noreply.github.com>
2024-05-04 18:45:21 +0530

    Update script.js

--- account/script.js
+++ account/script.js
@@ -1,1 +1,4 @@
-console.log("hello world!")
+Console.log("Donot send any info shown in the delevoper tools, Your account might be at risk of being hacked if you do so.")
+var database=firebase.database();
+var token=localStorage.getItem("authid")
+console.log("Connecting to the auth id ::::: "+token+" (Donot copy this!!!!!)")
