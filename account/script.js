9aae2b27173697524ca106b793951c99015e901d
Pushkar Sharma <80835222+sharmapushkar-coder@users.noreply.github.com>
2024-05-05 22:59:01 +0530

    Update script.js

--- account/script.js
+++ account/script.js
@@ -1,14 +1,25 @@
 console.log("Donot send any info shown in the delevoper tools, Your account might be at the risk of being hacked if you do so.")
 var database=firebase.database();
 var token=localStorage.getItem("authid")
-console.log("Connecting to the auth id ::::: "+token+" (Donot copy this!!!!!)")
-var user="Not found <a href=''>Try Again</a>"
-setTimeout(function(){
-  database.ref("login/"+token).on("value",function(snap){
-    user=snap.val().user
-  })
-  document.getElementById("name").innerHTML="<span class='label'>NAME:</span>"+user
-},6000)
+
+if(typeof token==="undefined"||token===""||token===null){
+  location.href="https://sharmapushkar-coder.github.io/socioai/login/?continue=https://sharmapushkar-coder.github.io/socioai/account"
+}
+else{
+  console.log("Connecting to the auth id ::::: "+token+" (Donot copy this!!!!!)")
+  var user="Not found <a href=''>Try Again</a>"
+  setTimeout(function(){
+    database.ref("login/"+token).on("value",function(snap){
+      user=snap.val().user
+    })
+    document.getElementById("name").innerHTML="<span class='label'>NAME:</span>"+user
+  },6000)
+}
+
+
+
+
+
 setTimeout(function() {
     var loadingDiv = document.getElementById('loading-div');
     loadingDiv.style.display = 'none';
