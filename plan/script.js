036f6c20e75c6de317951d47c8e0664238aa7caf
Pushkar Sharma <80835222+sharmapushkar-coder@users.noreply.github.com>
2024-05-09 07:32:19 +0530

    Update script.js

--- plan/script.js
+++ plan/script.js
@@ -4,3 +4,7 @@ document.querySelectorAll('.buy-btn').forEach(btn => {
     alert('Thank you for purchasing!');
   });
 });
+function buy(){
+  alert("we are redirecting you to our whats app number. Please send your username+plan name i.e. example@socioai gold.make sure you have a whats app downloaded on your phone. Once this procedure is completed you will receive a upi id or qr code, scan it and pay your amount. NOTE:: It may take atleast 30 min to show your current plan on the website. ")
+  location.href="https://wa.me/918377887193"
+}
