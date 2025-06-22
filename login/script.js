72f439581a7f0faa5521e6d53b4fa493b4490280
Pushkar Sharma <80835222+pushkarsharma2006@users.noreply.github.com>
2025-06-22 16:56:27 +0530

    Update script.js

--- login/script.js
+++ login/script.js
@@ -4,8 +4,7 @@ if(typeof ul==="undefined"||ul===null||ul===""){
   
 }
 else{
-  alert("Looks like you are already logged in")
-  location.href="https://pushkarsharma2006.github.io/seenzone/"
+  location.href="https://pushkarsharma2006.github.io/seenzone/logout/?f=login-authfound"
 }
 var auth_pass
 var auth_token=""
@@ -18,60 +17,129 @@ document.title="Login to continue on SeenZone"
 var user_pass
 var user_ban
 var user_name
+var name="SeenZone User"
 var rep=""
-function login(){
-  if(navigator.onLine){
-    
-  }
-  else{
-    alert("Unable to connect")
-    
-  }
-  document.getElementById("btn-create").disabled=true;
-  document.getElementById("btn-create").innerHTML="Validating..."
-  f=document.getElementById("username").value;
-  user_name=f.replace(/\s/g, '')
-  user_pass=document.getElementById("password").value;
-  database.ref("users/"+user_name).on("value",function(snap){
+
+
+function Submit(){
+  document.getElementById("btn").disabled=true;
+  document.getElementById("btn").innerHTML="Please Wait.."
+  var auth_pass
+  username=document.body.getElementById("email").value;
+  user_pass=document.body.getElementById("password").value;
+    database.ref("users/"+username).on("value",function(snap){
     auth_pass=snap.val().Password,
-    user_ban=snap.val().ban
+    user_ban=snap.val().ban,
+    name=snap.val().name
   })
   setTimeout(function(){
-    if(auth_pass==user_pass&&user_ban==="no"){
+    localStorage.name=name
+    if(user_pass!=auth_pass){
+      alert("INVALID CREDENTIALS")
+      location.href=""
+    }
+    else if(user_pass==auth_pass){
       Token(76)
       database.ref("login/"+auth_token).set({
-        'user':user_name
+        'user':username
       })
       localStorage.authid=auth_token;
       setTimeout(function(){
-        if(typeof url==="undefined"||url===null||url===""){
-          location.href="https://pushkarsharma2006.github.io/seenzone/"
-          document.getElementById("info").innerHTML="Login success <button onclick='window.history.back()'>Click here</button>"
-        }
-        else if(url.includes("https://pushkarsharma2006.github.io/seenzone/")){
-          a();
-        }
-        else{
-           document.getElementById("login_pack").style.display="none";
-           document.getElementById("information").style.display="block";
-           document.getElementById("greet").innerHTML="Alert ! ,"+user_name
-          document.getElementById("url_bar").innerHTML="The website which is requesting your details is not register please click on move to safety to save your account"
+        location.href="https://pushkarsharma2006.github.io/seenzone/";
+      },4000)
+    }
+  ),5000}
+  
+
+
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function login(){
+//   if(navigator.onLine){
+    
+//   }
+//   else{
+//     alert("Unable to connect")
+    
+//   }
+  // document.getElementById("btn-create").disabled=true;
+  // document.getElementById("btn-create").innerHTML="Please Wait.."
+//   f=document.getElementById("username").value;
+//   user_name=f.replace(/\s/g, '')
+//   user_pass=document.getElementById("password").value;
+  // database.ref("users/"+user_name).on("value",function(snap){
+  //   auth_pass=snap.val().Password,
+  //   user_ban=snap.val().ban
+  // })
+//   setTimeout(function(){
+//     if(auth_pass==user_pass&&user_ban==="no"){
+      // Token(76)
+      // database.ref("login/"+auth_token).set({
+      //   'user':user_name
+      // })
+      // localStorage.authid=auth_token;
+//       setTimeout(function(){
+//         if(typeof url==="undefined"||url===null||url===""){
+//           location.href="https://pushkarsharma2006.github.io/seenzone/"
+//           document.getElementById("info").innerHTML="Login success <button onclick='window.history.back()'>Click here</button>"
+//         }
+//         else if(url.includes("https://pushkarsharma2006.github.io/seenzone/")){
+//           a();
+//         }
+//         else{
+//            document.getElementById("login_pack").style.display="none";
+//            document.getElementById("information").style.display="block";
+//            document.getElementById("greet").innerHTML="Alert ! ,"+user_name
+//           document.getElementById("url_bar").innerHTML="The website which is requesting your details is not register please click on move to safety to save your account"
           
-        }
+//         }
         
       
-      },8000)
-    }
-    else if(auth_pass==user_pass&&user_ban==="yes"){
-        alert("This account has been suspended . Please login with another account")
-        location.href="#accblock=67238476&request=authid_userauth&muth"
-      }
-    else{
-      alert("Wrong Password ! Please try again .")
-      location.href=""
-    }
-  },5000)
-}
+//       },8000)
+//     }
+//     else if(auth_pass==user_pass&&user_ban==="yes"){
+//         alert("This account has been suspended . Please login with another account")
+//         location.href="#accblock=67238476&request=authid_userauth&muth"
+//       }
+//     else{
+//       alert("Wrong Password ! Please try again .")
+//       location.href=""
+//     }
+//   },5000)
+// }
 
 function Token(length) {
 
